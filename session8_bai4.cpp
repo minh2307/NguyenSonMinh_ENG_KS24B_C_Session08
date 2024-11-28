15a4a8def41edc333c9afc9f9eead042611cf2f7
minh2307 <minh23923@gmail.com>
2024-11-28 23:10:21 +0700

    session8_bai4

---
+++ session8_bai4.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int a[3][3] = {{23,54,6},{2,56,33},{28,94,82}},max;
+		max=a[0][0];
+	for(int i=0; i < sizeof(a)/sizeof(a[0]); i++){
+		for(int f=0; f < sizeof(a[i])/sizeof(int); f++){
+			if(max < a[i][f]){
+				max = a[i][f];
+			}
+		}
+	}
+	printf("phan tu lon nhat trong mang la: %d",max);
+	
+return 0;
+}
