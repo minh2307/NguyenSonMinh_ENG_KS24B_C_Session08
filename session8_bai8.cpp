ec8af7c9aa59574fe4dab2c22b58ce064f57e3b3
minh2307 <minh23923@gmail.com>
2024-11-29 09:39:30 +0700

    session8_bai8.cpp

---
+++ session8_bai8.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	int m,a[1000][1000];	
+		scanf("%d",&m);
+	for(int i=0; i < m ; i++){
+		for(int j=0; j < m; j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	int t=0;
+	for(int i=0; i < m; i++){
+		for(int j = m-1; j >= 0 ; j--){
+			printf("%d ",a[i][j]);
+			t+=a[i][j];
+		}
+	}
+	printf("\n%d",t);
+}
